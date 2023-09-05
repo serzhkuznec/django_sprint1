fcf1f293e7a5f91786e8c3cfd7237959c1932af7
Сергей Кузнецов <serzh.kuznec@bk.ru>
2023-09-05 16:45:54 +0400

    maybe

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'pages'
 
 urlpatterns = [
-    # Напишите адрес тут
     path('about/', views.about, name='about'),
     path('rules/', views.rules, name='rules'),
 ]
